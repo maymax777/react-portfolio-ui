fbc4d2c408a95bc5a85e48f71b4f9d324f65802d
maymax777 <dev.bugger777@yahoo.com>
2020-10-11 01:30:50 +1000

    Add Header to App

--- src/containers/App/index.js
+++ src/containers/App/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './index.css';
+import Header from 'components/layout/Header'
 
 function App() {
   return (
     <div className="App" id="name">
-      
+      <Header />
     </div>
   );
 }

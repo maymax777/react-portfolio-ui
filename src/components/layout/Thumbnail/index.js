15dd85be0641d63c05056718bd61c48785eabb86
maymax777 <dev.bugger777@yahoo.com>
2020-10-11 02:53:58 +1000

    Update Thumbnail component, import constants

--- src/components/layout/Thumbnail/index.js
+++ src/components/layout/Thumbnail/index.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import Article from 'components/common/Article';
+import { articles } from 'utils/constants.js';
 
 function Thumbnail() {
     return (
         <div>
-            
+            {articles.map(article => <Article {...article}/>)}
         </div>
     )
 }

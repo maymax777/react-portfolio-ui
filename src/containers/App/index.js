e2c492eb1b2d5161a30025e964ea853bcfdb2b46
maymax777 <dev.bugger777@yahoo.com>
2020-10-11 01:34:24 +1000

    Add Footer to App

--- src/containers/App/index.js
+++ src/containers/App/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './index.css';
-import Header from 'components/layout/Header'
+import Header from 'components/layout/Header';
+import Footer from 'components/layout/Footer';
 
 function App() {
   return (
     <div className="App" id="name">
       <Header />
+      <Footer />
     </div>
   );
 }

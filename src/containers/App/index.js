b4be4651c33ebf6282739504174209b6d1ad449d
maymax777 <dev.bugger777@yahoo.com>
2020-10-11 02:59:00 +1000

    Add custom fonts, styles

--- src/containers/App/index.js
+++ src/containers/App/index.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import './index.css';
+import 'assets/css/main.css';
 import Header from 'components/layout/Header';
 import Footer from 'components/layout/Footer';
+import Thumbnail from 'components/layout/Thumbnail';
 
 function App() {
   return (
     <div className="App" id="name">
       <Header />
+      <Thumbnail />
       <Footer />
     </div>
   );

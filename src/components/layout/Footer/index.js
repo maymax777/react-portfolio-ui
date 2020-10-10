e7469e58711980a78dccac21484d29acc40b9ce9
maymax777 <dev.bugger777@yahoo.com>
2020-10-11 01:33:20 +1000

    Add Footer component

---
+++ src/components/layout/Footer/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Footer() {
+    return (
+        <footer id="footer">
+            <ul class="copyright text-center">
+                <li>&copy; M@YM@X</li>
+                <li>Design: <a href="https://github.com/maymax777">Themewagon</a></li>                
+            </ul>
+        </footer>
+    )
+}
+
+export default Footer

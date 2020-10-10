77ab1960dd73ecbb9c3f5dee14f477fc1be2f87f
maymax777 <dev.bugger777@yahoo.com>
2020-10-11 02:47:01 +1000

    Update constants

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,8 +1,74 @@
 export const articles = [
     {
+        url: "images/fulls/01.jpg",
+        image: "images/thumbs/01.jpg",
+        title: "Carp , Nature , Slippers",
+        subtitle: "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+    },
+    {
         url: "images/fulls/02.jpg",
         image: "images/thumbs/02.jpg",
-        title: "",
-        subtitle: ""
+        title: "Carp , Nature , Slippers",
+        subtitle: "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+    },
+    {
+        url: "images/fulls/03.jpg",
+        image: "images/thumbs/03.jpg",
+        title: "Carp , Nature , Slippers",
+        subtitle: "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+    },
+    {
+        url: "images/fulls/04.jpg",
+        image: "images/thumbs/04.jpg",
+        title: "Carp , Nature , Slippers",
+        subtitle: "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+    },
+    {
+        url: "images/fulls/05.jpg",
+        image: "images/thumbs/05.jpg",
+        title: "Carp , Nature , Slippers",
+        subtitle: "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+    },
+    {
+        url: "images/fulls/06.jpg",
+        image: "images/thumbs/06.jpg",
+        title: "Carp , Nature , Slippers",
+        subtitle: "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+    },
+    {
+        url: "images/fulls/07.jpg",
+        image: "images/thumbs/07.jpg",
+        title: "Carp , Nature , Slippers",
+        subtitle: "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+    },
+    {
+        url: "images/fulls/08.jpg",
+        image: "images/thumbs/08.jpg",
+        title: "Carp , Nature , Slippers",
+        subtitle: "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+    },
+    {
+        url: "images/fulls/09.jpg",
+        image: "images/thumbs/09.jpg",
+        title: "Carp , Nature , Slippers",
+        subtitle: "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+    },
+    {
+        url: "images/fulls/10.jpg",
+        image: "images/thumbs/10.jpg",
+        title: "Carp , Nature , Slippers",
+        subtitle: "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+    },
+    {
+        url: "images/fulls/11.jpg",
+        image: "images/thumbs/11.jpg",
+        title: "Carp , Nature , Slippers",
+        subtitle: "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+    },
+    {
+        url: "images/fulls/12.jpg",
+        image: "images/thumbs/12.jpg",
+        title: "Carp , Nature , Slippers",
+        subtitle: "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
     }
 ];

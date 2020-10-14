9a55802277f0efcb57ff855e3a2b0bcce8eb1c15
maymax777 <dev.bugger777@yahoo.com>
2020-10-14 11:28:09 -0400

    Add initial Carousel component

--- src/containers/App/index.js
+++ src/containers/App/index.js
@@ -1,16 +1,10 @@
 import React from 'react';
 import './index.css';
-import 'assets/css/main.css';
-import Header from 'components/layout/Header';
-import Footer from 'components/layout/Footer';
-import Thumbnail from 'components/layout/Thumbnail';
 
 function App() {
   return (
     <div id="main">
-      <Header />
-      <Thumbnail />
-      <Footer />
+      
     </div>
   );
 }
